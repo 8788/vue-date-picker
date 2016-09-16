ca82a8a040f03504a7afb54c15bd0a71653d3ce7
Michael Teeuw <michael@xonaymedia.nl>
2016-09-16 10:58:51 +0200

    Add name to example.

--- src/demo.vue
+++ src/demo.vue
@@ -5,7 +5,7 @@
 <template>
     <datepicker :readonly="true" format="YYYY-MM-DD"></datepicker>
     <datepicker format="YYYY-M-D" value="2015-9-5"></datepicker>
-    <datepicker :readonly="true" format="MMM/D/YYYY" width="300px"></datepicker>
+    <datepicker :readonly="true" format="MMM/D/YYYY" width="300px" name="date"></datepicker>
 </template>
 
 <script>
