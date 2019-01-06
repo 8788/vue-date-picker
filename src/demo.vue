9caa2b245176c7ab651fc52e69235ab218401775
8788 <2802513+8788@users.noreply.github.com>
2019-01-06 20:22:54 +0800

    update demo and screenshot

--- src/demo.vue
+++ src/demo.vue
@@ -1,7 +1,13 @@
+<style>
+.demo > * {
+  margin-right: 10px;
+}
+</style>
+
 <template>
-  <div>
+  <div class="demo">
     <datepicker :readonly="true" format="YYYY-MM-DD" name="date1"></datepicker>
-    <datepicker value="2015-9-5" format="YYYY-M-D" name="date2"></datepicker>
+    <datepicker value="2018-9-5" format="YYYY-M-D" name="date2"></datepicker>
     <datepicker :readonly="true" format="MMM/D/YYYY" name="date3"></datepicker>
   </div>
 </template>
