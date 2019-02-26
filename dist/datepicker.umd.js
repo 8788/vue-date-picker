b8a7143860e1e90435096805d5d5cfa55b8a664c
8788 <2802513+8788@users.noreply.github.com>
2019-02-26 21:32:26 +0800

    update output file name

--- dist/datepicker.umd.js
+++ dist/datepicker.umd.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["vue-date-picker"] = factory();
+		exports["datepicker"] = factory();
 	else
-		root["vue-date-picker"] = factory();
+		root["datepicker"] = factory();
 })((typeof self !== 'undefined' ? self : this), function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
